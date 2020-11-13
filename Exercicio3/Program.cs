1abfa03567f00cf415d63bf99e67261bab330913
DEV <dev@info.local>
2020-11-13 10:37:27 -0300

    Exercício 3 Finalizado

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu salário em R$: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if(salario<500.00){
+                double novoSalario = (0.3*salario) + salario;
+                Console.WriteLine(novoSalario);
+                
+            }
+        }
+    }
+}
