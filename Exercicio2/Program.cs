6d5b648f41d316d93e69c26ac3798397d40dd705
DEV <dev@info.local>
2020-11-13 09:48:36 -0300

    Exercício 2 finalizado

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento, anoAtual, idade;
+            double semanas;
+            Console.Write(" Digite o ano do seu nascimento: ");
+            anoNascimento = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o ano da data atual: ");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            idade = anoAtual-anoNascimento;
+            Console.WriteLine("Sua idade é " + idade + " anos.");
+
+            semanas = idade * 52.1429;
+            Console.WriteLine("Sua idade em semanas é " + semanas + " semanas.");
+
+
+
+
+        }
+    }
+}
