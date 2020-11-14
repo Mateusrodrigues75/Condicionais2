0c258aaa4754d873429fc8458cd1b8e4bb10533a
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-14 11:26:18 -0300

    Desafio finalizado

---
+++ Desafio 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.Write("Qual sua Idade? ");
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade >= 5 && idade <= 7){
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Sua categoria de Nadador é Infantil A.");
+            }
+            if(idade >= 8 && idade <= 10){
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Sua categoria de Nadador é Infantil B.");
+            }
+            if(idade >= 11 && idade <= 13){
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Sua categoria de Nadador é Juvenil A.");
+            }
+            if(idade >= 14 && idade <= 17){
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Sua categoria de Nadador é Juvenil B.");
+            }
+            if(idade >=18){
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Sua categoria de Nadador é Sênior.");
+            }
+        }
+    }
+}
